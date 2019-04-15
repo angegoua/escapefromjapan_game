8c0f8e7ab23afe30f0d99af7b83fff3404f94780
cgillet <constant.gillet@hetic.net>
2019-04-15 17:00:13 +0200

    Test image

--- scripts/game.js
+++ scripts/game.js
@@ -1,55 +1,27 @@
-var canvas = document.querySelector('#game')
-var ctx = canvas.getContext("2d")
+const canvas = document.querySelector('#game')
+const ctx = canvas.getContext("2d")
 
+class Player {
+    constructor(posX, posY, direction, speed, skin) {
 
-function loadImage(url) {
-    return new Promise(resolve => {
-        const image = new Image();
-        image.addEventListener('load', () => {
-            resolve(image);
-        });
-        image.src = url;
-    });
+        this.posX = posX; //Position du joueur
+        this.posY = posY;   //Position du joueur Y
+        this.direction = direction; //Direction du joueur 
+        this.speed = speed; //Vitesse du joueur
+        this.skin = skin;
+    }
+    
+    createPlayer(){
+
+        console.log('ok')
+        let image = new Image()
+        image.src = 'images/player.png'
+        
+        ctx.drawImage(image, this.posX, this.posY) 
+    }
+    
 }
 
+let player = new Player(40, 100, 'down', 1, 'images/player.png')
 
-loadImage('images/tile1.png')
-
-class SpriteSheet {
-    constructor(image, w = 40, h = 40) {
-        this.image = image;
-        this.width = w;
-        this.height = h;
-        this.tiles = new Map();
-    }
-    define(name, x, y) {
-        const buffer = document.createElement('canvas');
-        buffer.height = this.height;
-        buffer.width = this.width;
-        buffer
-            .getContext('2d')
-            .drawImage(
-                this.image,
-                this.width * x,
-                this.height * y,
-                this.width,
-                this.height,
-                0,
-                0,
-                this.width,
-                this.height);
-        this.tiles.set(name, buffer);
-    }
-
-    draw(name, context, x, y) {
-        const buffer = this.tiles.get(name);
-        context.drawImage(buffer, x, y);
-    }
-
-    drawTile(name, context, x, y) {
-        this.draw(name, context, x * this.width, y * this.height);
-    }
-}
-
-const sprites = new SpriteSheet(image, 40,40)
-
+player.createPlayer()
