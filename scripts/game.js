302f855a5ffd9afa62efc68608fd75e4da0bcd86
Ange Goua <ange.goua01@gmail.com>
2019-04-15 15:13:17 +0200

    Class SpriteSheet

--- scripts/game.js
+++ scripts/game.js
@@ -1,16 +1,57 @@
 var canvas = document.querySelector('#game')
 var ctx = canvas.getContext("2d")
 
-ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+import loaders from './loaders.js'
+
+class SpriteSheet {
+    constructor(image, w = 16, h = 16) {
+        this.image = image;
+        this.width = w;
+        this.height = h;
+        this.tiles = new Map();
+    }
+
+    define(name, x, y) {
+        const buffer = document.createElement('canvas');
+        buffer.height = this.height;
+        buffer.width = this.width;
+        buffer
+            .getContext('2d')
+            .drawImage(
+                this.image,
+                this.width * x,
+                this.height * y,
+                this.width,
+                this.height,
+                0,
+                0,
+                this.width,
+                this.height);
+        this.tiles.set(name, buffer);
+    }
+
+    draw(name, context, x, y) {
+        const buffer = this.tiles.get(name);
+        context.drawImage(buffer, x, y);
+    }
+
+    drawTile(name, context, x, y) {
+        this.draw(name, context, x * this.width, y * this.height);
+    }
+}
+
+const sprites = new SpriteSheet()
+
+// ctx.beginPath();
+// ctx.rect(20, 40, 50, 50);
+// ctx.fillStyle = "#FF0000";
+// ctx.fill();
+// ctx.closePath();
 
 
 
-ctx.beginPath();
-ctx.rect(100, 40, 50, 50);
-ctx.fillStyle = "#FF0000";
-ctx.fill();
-ctx.closePath();
+// ctx.beginPath();
+// ctx.rect(100, 40, 50, 50);
+// ctx.fillStyle = "#FF0000";
+// ctx.fill();
+// ctx.closePath();
