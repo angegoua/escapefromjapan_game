41b9b1096653dcaa539dc772425a7eb4aee2b4e2
cgillet <constant.gillet@hetic.net>
2019-04-15 14:12:22 +0200

    Update game.js

--- scripts/game.js
+++ scripts/game.js
@@ -2,7 +2,7 @@ var canvas = document.querySelector('#game')
 var ctx = canvas.getContext("2d")
 
 ctx.beginPath();
-ctx.rect(20, 40, 50, 50);
+ctx.rect(20, 40, 40, 40);
 ctx.fillStyle = "#FF0000";
 ctx.fill();
 ctx.closePath();
