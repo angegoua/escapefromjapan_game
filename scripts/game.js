67cb51cf049054376f6758d8e1b6092f8183819a
Ange Goua <ange.goua01@gmail.com>
2019-04-15 17:57:42 +0200

    Player Move

--- scripts/game.js
+++ scripts/game.js
@@ -1,6 +1,7 @@
 const canvas = document.querySelector('#game')
 const ctx = canvas.getContext("2d")
 
+
 class Player {
     constructor(posX, posY, direction, speed, skin) {
 
@@ -8,20 +9,36 @@ class Player {
         this.posY = posY;   //Position du joueur Y
         this.direction = direction; //Direction du joueur 
         this.speed = speed; //Vitesse du joueur
-        this.skin = skin;
+        this.skin = skin; //Personnage
     }
     
-    createPlayer(){
+    movePlayer(){
+        canvas.addEventListener('click',function(){
 
-        console.log('ok')
-        let image = new Image()
-        image.src = 'images/player.png'
-        
-        ctx.drawImage(image, this.posX, this.posY) 
+            ctx.clearRect(player.posX, player.posY, 40,40)
+            player.posX = player.posX + 40 // + largeur du player
+            
+            ctx.drawImage(image, player.posX, player.posY)
+            image.src = player.skin 
+
+        })
     }
-    
 }
 
-let player = new Player(40, 100, 'down', 1, 'images/player.png')
+let player = new Player(0, 691, 'down', 1, 'images/player.png')
 
-player.createPlayer()
+//PLAYER'S CREATE
+let image = new Image()
+
+image.onload = function(){
+    ctx.drawImage(image, player.posX, player.posY)
+}
+
+image.src = player.skin 
+
+
+//PLAYER'S MOVE
+player.movePlayer()
+
+
+
